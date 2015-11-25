7c5a73eaf0c701c4ccf5b9d7a2ceac6faefd39ed
elsehow <yes@cosmopol.is>
2015-11-24 17:29:35 -0800

    lots of progress streamifying and testing the store

--- src/NotesStore.js
+++ src/NotesStore.js
@@ -1,56 +1,77 @@
 var Kefir = require('kefir')
   path = require('path'),
   search  = require('and-search'),
-  keywords = require('./helpers/keywords.js')
-
-// functions that act on the state
-
-function selectionByIndex (selectionState, i, visibleNotes) {
-  return {
-    selectionIndex: i,
-    selectionValue: visibleNotes[i-1]
-  }
-}
-
-function showSearchResults (state) {
-  var kwords = keywords(state.searchState.textboxValue)
-  // search for notes if there are keywords
-  if (kwords) {
-    var searchResults = search(state.notesState.allNotes, kwords)
-  } else {
-  // otherwise display all notes
-  }
-}
+  keywords = require('./helpers/keywords.js'),
+  _ = require('lodash')
 
 function one () { return 1 }
 function minusOne () { return -1 }
 function sum (a,b) { return a+b }
 function emptyString (a,b) { return '' }
 function concat (acc, cur) { return acc += cur }
-function removeLast (item) { return item }
+var backspaceKeyword = 'BKSP'
+var clearKeyword = 'CLR'
+function intoBackspaceKeyword () { return backspaceKeyword }
+function intoClearKeyword() { return clearKeyword }
+function concatTextbox (acc, cur) {
+  if (cur === backspaceKeyword)
+    return _.initial(acc).join('')
+  if (cur === clearKeyword)
+    return ''
+  return concat(acc, cur)
+}
+function asKey (k) {
+  return function (v) {
+    return [k, v]
+  }
+}
+function searchIfNotes (notes, textboxVal) {
+  var kwords = keywords(textboxVal)
+  // search for notes if there are keywords
+  if (kwords) {
+    var res = search(notes, kwords)
+    return res
+}
+  return notes
+}
 
 function setup (dispatcher, notesDir) {
+
+  // our app state
+  var state = {}
+  // takes a values of the form [key, value]
+  function intoState (v) {
+    state[v[0]] = v[1]
+    return state
+  }
 
   function fullPath (filename) {
     return path.join(notesDir, filename)
   }
 
   var typedCharS        = Kefir.fromEvents(dispatcher, 'addToTextbox')
-  var backspaceTextboxS = Kefir.fromEvents(dispatcher, 'backspaceTextbox')
-  var clearTextboxS     = Kefir.fromEvents(dispatcher, 'clearTextbox')
+  var backspaceTextboxS = Kefir.fromEvents(dispatcher, 'backspaceTextbox').map(intoBackspaceKeyword)
+  var clearTextboxS     = Kefir.fromEvents(dispatcher, 'clearTextbox').map(intoClearKeyword)
   var scrollUpS         = Kefir.fromEvents(dispatcher, 'scrollUp').map(one)
   var scrollDownS       = Kefir.fromEvents(dispatcher, 'scrollDown').map(minusOne)
   var openSelectedNoteS = Kefir.fromEvents(dispatcher, 'openSelectedNote')
   var notesList         = Kefir.fromEvents(dispatcher, 'notesList')
 
-  var textboxValue      = typedCharS.scan(concat)
-                            //.combine(backspaceTextboxS, removeLast)
-                            //.merge(clearTextboxS)
+  var textboxValue      = typedCharS
+                            .merge(backspaceTextboxS)
+                            .merge(clearTextboxS)
+                            .scan(concatTextbox)
+                            .toProperty(function () { return '' })
 
   var scrollPos         = scrollUpS.combine(scrollDownS, sum)
 
-  return textboxValue
+  var displayedNotes    = notesList
+                            .combine(textboxValue, searchIfNotes)
 
+// TODO
+// scroll up and down the list
+// select something
+  
 //  state obj:
 //  we emit this to the renderer
 // 
@@ -59,6 +80,16 @@ function setup (dispatcher, notesDir) {
 //   displayedNotes
 //   selectionIndex
 // }
+
+  var stateStream = Kefir.merge([
+    textboxValue.map(asKey('textboxVal'))
+  , displayedNotes.map(asKey('displayedNotes'))
+  ]).map(intoState)
+                            
+                            
+
+
+  return stateStream
 
 }
 
--- test/test.js
+++ test/test.js
@@ -1,41 +1,83 @@
 var EventEmitter = require('events').EventEmitter
   , test = require('tape')
+  , _ = require('lodash')
 
 var store = require('../src/NotesStore.js')
 var dispatcher = new EventEmitter()
 var stateS = store(dispatcher)
 
+var fakeNotes = [
+  'very cool note',
+  'things good and pending',
+  'my client work',
+  'assorted notes on trout',
+  'mighty casey has just struck trout'
+]
 
-function testStream (t, answers, stream) {
+function testStream (t, answers, stream, prop) {
+  stream = stream.map(_.property(prop)).skipDuplicates()
   var i = 0
   function c (v) {
+    t.deepEquals(v, answers[i])
+    i+=1
     if (i == answers.length) {
       stream.offValue(c)
       t.end()
     }
-    t.equals(v, answers[i])
-    i+=1
   }
   stream.onValue(c)
 }
 
+
 test('adding chars to textbox', function (t) {
   var ans = [
-    'a', 'ab', 'abc'
+    '', 'a', 'ab', 'abc'
   ]
-  testStream(t, ans, stateS)
+  testStream(t, ans, stateS, 'textboxVal')
   dispatcher.emit('addToTextbox', 'a')
   dispatcher.emit('addToTextbox', 'b')
   dispatcher.emit('addToTextbox', 'c')
 })
 
-test('backspace chars to textbox', function (t) {
+test('backspace chars from textbox', function (t) {
   var ans = [
-    'abcde', 'abcdef'
+    'abc', 'abcd', 'abc'
   ]
-  testStream(t, ans, stateS)
-  //dispatcher.emit('addToTextbox', 'e')
-  //dispatcher.emit('addToTextbox', 'f')
+  testStream(t, ans, stateS, 'textboxVal')
+  dispatcher.emit('addToTextbox', 'd')
+  dispatcher.emit('backspaceTextbox')
 })
 
+test('clear textbox', function (t) {
+  var ans = [
+    'abc', '', 'a', ''
+  ]
+  testStream(t, ans, stateS, 'textboxVal')
+  dispatcher.emit('clearTextbox')
+  dispatcher.emit('addToTextbox', 'a')
+  dispatcher.emit('clearTextbox')
+})
 
+test('can take notes list', function (t) {
+  var ans = [
+    undefined, 
+    fakeNotes, 
+  ]
+  testStream(t, ans, stateS, 'displayedNotes')
+  dispatcher.emit('notesList', fakeNotes)
+})
+
+test('can search for notes', function (t) {
+  var ans = [
+    fakeNotes,
+    [ 'my client work',
+      'mighty casey has just struck trout'
+    ],
+    [ 'my client work'],
+    fakeNotes
+  ]
+  testStream(t, ans, stateS, 'displayedNotes')
+  dispatcher.emit('addToTextbox', 'm')
+  dispatcher.emit('addToTextbox', 'y')
+  dispatcher.emit('clearTextbox')
+})
